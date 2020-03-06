6f3a4e588625ef8fe373fff716cad9d64b918dc5
Антон <jungleraider@live.com>
2020-03-07 00:48:23 +0700

    Paginated page for displaying current user' pastas

---
+++ resources/views/snippets/list.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.base')
+@section('content')        
+
+<form method="post" action="{{ action('SnippetController@store') }}">
+                @csrf
+                <div class="form-group form-inline">
+                    <input disabled value="{{ $snippet->title ?? "Без названия" }}" type="text" name="title" id="title" class="form-control" placeholder="Название &laquo;пасты&raquo;" />                    
+                    <div class="dropdown">
+                        <div class="btn-group">
+                            <button class="btn dropdown-toggle disabled" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                                
+                                @if ($snippet->expired_at)
+                                    Срок годности истекает: <b>{{ $snippet->expired_at }}</b>
+                                @else                                                                    
+                                    Срок годности: <b>∞</b>
+                                @endisset                                                         
+                                
+                                
+                            </button>
+                        </div>
+                        <div class="btn-group">
+                            <button class="btn dropdown-toggle disabled" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Доступ: <b>{{ $snippet->accessMode->title }}</b>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <textarea readonly required name="content" class="form-control" id="pastaArea" rows="3">{{ $snippet->content }}</textarea>
+            </form>
+                    
+                    
+@endsection
